cb40d854a5616e1267824f37c951cec9b53739e1
iannunez-max <iannumi24@alumnes.ilerna.com>
2026-05-10 21:32:41 +0200

    acabado

--- android/app/src/main/kotlin/com/example/instadam_grupo10/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/instadam_grupo10/MainActivity.kt
@@ -1,5 +1,7 @@
-package com.instadam
+// Este archivo es antiguo y ha sido reemplazado por com/instadam/MainActivity.kt
+// Se ha cambiado el nombre del paquete para evitar el error de Redeclaration.
+package com.example.instadam_grupo10_old
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivityOld : FlutterActivity()
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Iannumi24\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Iannumi24\Desktop\INSTA-DAM"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\iannu\OneDrive\Escritorio\INSTA-DAM"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
